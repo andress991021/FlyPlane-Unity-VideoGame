81ac43347526dc3b8f22594b6c232d8912480a5c
alejomaar <demonmaar@gmail.com>
2023-04-23 19:16:50 -0500

    feat: send vector movement to unity - refactor: improve variable names and delete examples files

--- backend/client.py
+++ backend/client.py
@@ -2,42 +2,46 @@
 from websockets.sync.client import connect
 import time
 import cv2
-from computer_vision import HandDetector,Dynamics    
+from computer_vision import HandDetector   
 from settings import settings
 
-host =  settings.host
-port = settings.port_bridge
+# Open the video capture using the camera
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)   
 
-
-cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)   
+# Initialize the hand detection class
 hand_detector = HandDetector()
-dynamics = Dynamics()
 
-def streaming():
+# Continuously detect the hand position and send it through websockets
+def stream_hand_position():
+    # Connect to websocket mediator
     with connect(f"ws://localhost:8000") as websocket:
         for i in range(500):
+            # Capture the image
             ret, frame = cap.read()
             frame = cv2.flip(frame,1)
             
-
+            # Detect the hand and its principal keypoints
             frame = hand_detector.find_hand(frame)   
-            vector = hand_detector.find_main_keypoint(frame,False) 
+            main_keypoint = hand_detector.find_main_keypoint(frame, False) 
             
-            if vector is not None:
-                
-                x = vector['x']
-                x_formatted = f'{x:4.2f}'
-                print(x_formatted)
-                websocket.send(x_formatted)
+            # If a hand position is found, send it through websockets
+            if main_keypoint is not None:
+                str_position = '{x:.4f},{y:.4f}'.format(**main_keypoint)
+                print(f'Sending hand position: {str_position}')
+                websocket.send(str_position)
             
-            cv2.imshow('mask',frame)
+            # Show the hand detection mask
+            cv2.imshow('Hand Detection Mask', frame)
+            
+            # Exit if the 'Esc' key is pressed
             if cv2.waitKey(1) & 0xFF == 27:
                 break 
             
             time.sleep(0)
-            
-
+        
+        # Release the video capture and destroy the window
         cap.release()     
         cv2.destroyAllWindows()
 
-streaming()
+# Start streaming the hand position
+stream_hand_position()
