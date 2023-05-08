d082813b397e642dc5be5d007309ba125bef9fe0
alejomaar <demonmaar@gmail.com>
2023-05-07 22:17:38 -0500

    feat: reduce movement noise if velocity is below of thresold

--- unity/Assets/Scripts/Movements.cs
+++ unity/Assets/Scripts/Movements.cs
@@ -11,9 +11,9 @@ using System.Globalization;
 
 public class Movements : MonoBehaviour, IEventListener
 {
-    const float vz = 15f;
+    const float vz = 35f;
     const float vhand = 1.8f;
-    float vhandThresold = 0.01f;
+    float vhandThresold = 0.008f;
     private DynamicsData lastDynamics = null;
     bool isTrigger = false;
 
@@ -28,18 +28,32 @@ public class Movements : MonoBehaviour, IEventListener
         GlobalStorage.eventManager.suscribe(this);
     }
 
+    void moveWithHand() {
+        if (!isTrigger || lastDynamics == null) return;
+
+        float vx = lastDynamics.vx;
+        float vy = lastDynamics.vy;
+        if (Mathf.Abs(vx) < vhandThresold) vx = 0;
+        if (Mathf.Abs(vy) < vhandThresold) vy = 0;
+        Debug.Log(vx + "  ,  " + vy);
+
+        Vector3 movement = -transform.forward * vhand * vx - transform.up * vhand * vy + transform.right * vz * Time.deltaTime;
+        transform.Translate(movement);
+        isTrigger = false;
+    }
+
+    void moveWithKeyboard() {
+        float x = Input.GetAxis("Horizontal") * vz;
+        float z = 8f;
+        float y = Input.GetAxis("Vertical") * vz;
+        Vector3 movement = transform.right * x + transform.forward * z + transform.up * y;
+        transform.Translate(movement * Time.deltaTime);
+    }
+
 
     void Update()
     {
-        if (!isTrigger || lastDynamics==null) return;
-
-        float px = lastDynamics.px;
-        float py = lastDynamics.py;
-        float vx = lastDynamics.vx;
-        float vy = lastDynamics.vy;
-
-        Vector3 movement = -transform.forward  * vhand * vx - transform.up * vhand * vy + transform.right * vz * Time.deltaTime;
-        transform.Translate(movement);
-        isTrigger = false;
+        moveWithHand();//  
+        //moveWithKeyboard() //Uncomment this for move the gameobject using the keyboards
     }
 }
