e09446c7f37f4fc8d20694841aa920ed49c400e2
alejomaar <demonmaar@gmail.com>
2023-05-07 23:04:58 -0500

    feat: smooth movement using weight filtering

--- backend/computer_vision/dynamics.py
+++ backend/computer_vision/dynamics.py
@@ -4,24 +4,28 @@ import numpy as np
 from numpy import array
      
 class Dynamics():
+    
     def __init__(self):
-        self.position = []
+        self.position = np.empty((0, 2))
+        self.velocity = np.empty((0, 2))
         self.time = []
-        self.velocity = []
+        self.kernel = np.array([0.18, 0.32, 0.5])
         
     def add_vector(self,new_vector:array):
-        self.position.append(new_vector)
+        t = datetime.now()        
+        if self.position.size > 0:
+            t0 = self.time[-1]
+            dt = (t - t0).total_seconds()
+            v = (new_vector - self.position[-1]) / dt
+        else:
+            v = np.zeros(2)
+        self.position = np.append(self.position, [new_vector], axis=0)
         self.time.append(datetime.now()) 
+        self.velocity = np.append(self.velocity, [v], axis=0)
         
         
     def calculate_velocity(self):
-        if len(self.position)<3:
+        if self.position.shape[0] < 4:
             return (0,0)
-        dt_1 = (self.time[-1]- self.time[-2]).total_seconds()
-        dt_2 = (self.time[-2]- self.time[-3]).total_seconds()
-        
-        v_1 = (self.position[-1]- self.position[-2])/dt_1
-        v_2 = (self.position[-2]- self.position[-3])/dt_2
-        vx,vy = v_1*0.6+v_2*0.4
-        return vx,vy
+        return self.kernel@self.velocity[-3:]
     
