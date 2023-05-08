3fde4bf5306cee892668dada1c923d892f6c10c3
alejomaar <demonmaar@gmail.com>
2023-05-07 22:45:36 -0500

    feat: smooth movement using weight filtering

--- backend/computer_vision/dynamics.py
+++ backend/computer_vision/dynamics.py
@@ -1,29 +1,27 @@
 from datetime import datetime, timedelta
 from typing import Dict, Tuple
+import numpy as np
 from numpy import array
      
 class Dynamics():
     def __init__(self):
-        self.vector_old: array = None
-        self.vector_current: array = None
-        self.time_old: datetime = None
-        self.time_current: datetime = None
+        self.position = []
+        self.time = []
+        self.velocity = []
         
     def add_vector(self,new_vector:array):
-        
-        self.vector_old = self.vector_current
-        self.time_old = self.time_current
-        
-        self.vector_current = new_vector
-        self.time_current = datetime.now()
+        self.position.append(new_vector)
+        self.time.append(datetime.now()) 
         
         
     def calculate_velocity(self):
-        if self.vector_old is None or self.vector_current is None:
+        if len(self.position)<3:
             return (0,0)
-        dt = (self.time_current - self.time_old).total_seconds()
-        vx,vy = (self.vector_current- self.vector_old)/dt
+        dt_1 = (self.time[-1]- self.time[-2]).total_seconds()
+        dt_2 = (self.time[-2]- self.time[-3]).total_seconds()
+        
+        v_1 = (self.position[-1]- self.position[-2])/dt_1
+        v_2 = (self.position[-2]- self.position[-3])/dt_2
+        vx,vy = v_1*0.6+v_2*0.4
         return vx,vy
     
-    def is_empty_vectors(self):
-        return self.vector_old is None and self.vector_current is None
